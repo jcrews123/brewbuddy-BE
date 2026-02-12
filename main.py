de0e9b0cd8e09328cd26a483cee02ccd8d744479
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2026-02-11 19:41:50 -0500

    removed vercel.json and renamed/moved app.py to main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_jwt_extended import JWTManager
-from api.utils import APIException, generate_sitemap
-from api.models import db
-from api.routes import api
-from api.admin import setup_admin
-from api.commands import setup_commands
+from src.api.utils import APIException, generate_sitemap
+from src.api.models import db
+from src.api.routes import api
+from src.api.admin import setup_admin
+from src.api.commands import setup_commands
 
 # from models import Person
 
@@ -84,6 +84,6 @@ def serve_any_other_file(path):
 
 
 # this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+# if __name__ == '__main__':
+#     PORT = int(os.environ.get('PORT', 3001))
+#     app.run(host='0.0.0.0', port=PORT, debug=True)
